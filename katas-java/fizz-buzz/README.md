fba48414b04fae7fd8d4c366bc4282e46c9a4dfb
Jose Ignacio Dominguez Montoya <jose.ignacio.dominguez@clarity.ai>
2021-01-14 15:57:31 +0100

    [GildeRose] Create tests

--- katas-java/fizz-buzz/README.md
+++ katas-java/fizz-buzz/README.md
@@ -1,3 +1,4 @@
+
 # Goal
 
 Write a program that returns the numbers from 1 to 100. But for multiples of three return "Fizz" instead of the number and for the multiples of five return "Buzz". For numbers which are multiples of both three and five return "FizzBuzz".
--- katas-java/gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ katas-java/gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,17 +2,129 @@ package com.gildedrose;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class GildedRoseTest {
+class GildedRoseTest {
 
   @Test
-  public void fix_me() {
-    Item[] items = new Item[]{new Item("foo", 0, 0)};
+  void shouldUpdateItemWhenItIsARegularItem() {
+    Item[] items = new Item[]{new Item("regularItem", 5, 5)};
     GildedRose app = new GildedRose(items);
 
     app.updateQuality();
+    Item actual = app.items[0];
 
-    assertEquals("fixme", app.items[0].name);
+    assertEquals("regularItem", actual.name);
+    assertEquals(4, actual.sellIn);
+    assertEquals(4, actual.quality);
   }
+
+  @Test
+  void shouldNotUpdateQualityWhenItIsAlreadyZero() {
+    Item[] items = new Item[]{new Item("regularItem", 5, 0)};
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+    Item actual = app.items[0];
+
+    assertEquals(4, actual.sellIn);
+    assertEquals(0, actual.quality);
+  }
+
+  @Test
+  void shouldDoubleDegradeQualityWhenTheSellDateHasPassed() {
+    Item[] items = new Item[]{new Item("regularItem", 0, 10)};
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+    Item actual = app.items[0];
+
+    assertEquals(-1, actual.sellIn);
+    assertEquals(8, actual.quality);
+  }
+
+
+  @Test
+  void shouldUpdateQuality1WhenIsAnAgedBrieWithPositiveSellIn() {
+    Item[] items = new Item[]{new Item("Aged Brie", 2, 2)};
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+    Item actual = app.items[0];
+
+    assertEquals("Aged Brie", actual.name);
+    assertEquals(1, actual.sellIn);
+    assertEquals(3, actual.quality);
+  }
+
+
+  @Test
+  void shouldNotUpdateQuality2WhenIsAnAgedBrieWithZeroSellIn() {
+    Item[] items = new Item[]{new Item("Aged Brie", 0, 2)};
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+    Item actual = app.items[0];
+
+    assertEquals("Aged Brie", actual.name);
+    assertEquals(-1, actual.sellIn);
+    assertEquals(4, actual.quality);
+  }
+
+
+  @Test
+  void shouldNotUpdateQualityWhenTheQualityIsAlready50() {
+    Item[] items = new Item[]{new Item("Aged Brie", 5, 50)};
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+    Item actual = app.items[0];
+
+    assertEquals(4, actual.sellIn);
+    assertEquals(50, actual.quality);
+  }
+
+
+  @Test
+  void shouldNeverUpdateQualityWhenTheItemIsSulfuras() {
+    Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 5, 5)};
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+    Item actual = app.items[0];
+
+    assertEquals("Sulfuras, Hand of Ragnaros", actual.name);
+    assertEquals(5, actual.sellIn);
+    assertEquals(5, actual.quality);
+  }
+
+
+  @ParameterizedTest
+  @MethodSource("backstagePassesTestCases")
+  void shouldUpdateQualityWhenTheItemIsBackstagePasses(int sellin, int quality, int expectedSellin, int expectedQuality) {
+    Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", sellin, quality)};
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+    Item actual = app.items[0];
+
+    assertEquals(expectedSellin, actual.sellIn);
+    assertEquals(expectedQuality, actual.quality);
+  }
+
+  private static Stream<Arguments> backstagePassesTestCases() {
+
+    return Stream.of(
+      Arguments.of(11, 5, 10, 6),
+      Arguments.of(10, 5, 9, 7),
+      Arguments.of(5, 5, 4, 8),
+      Arguments.of(0, 5, -1, 0)
+    );
+  }
+
 }
